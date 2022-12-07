34ccba5b2f4251b838e427bde69b232482b93c9c
labs.pecggtu.152.11@outlook.com <labs.pecggtu.152.11@outlook.com>
2022-12-07 18:36:20 +0300

    модуль 1_2 демэкзамен

---
+++ avto_csv.py
@@ -0,0 +1,72 @@
+import csv
+
+csv_file = []
+
+
+# Открыть файл
+def file_open(file_name):
+    with open(file_name, newline='', encoding='utf-8') as file:
+        reader = csv.DictReader(file, delimiter=';')
+        for row in reader:
+            print(row)
+            csv_file.append(row)
+        print("Файл открыт. Записей: ", len(csv_file))
+
+
+# Добавление
+def insert(vin, nom, mod, age, mog, prob, hoz, st):
+    try:
+        csv_file.append({'vin': vin,
+                         'номер': nom,
+                         'модель': mod,
+                         'год': age,
+                         'мощность': mog,
+                         'пробег': prob,
+                         'владелец': hoz,
+                         'стоимость': st})
+    except Exception as e:
+        return f"Ошибка при добавлении новой записи {e}"
+    print("Данные добавлены")
+
+
+# Удаление
+def drop_by_arg(val, col_name="номер"):
+    global csv_file
+    try:
+        csv_file = list(filter(lambda x: x[col_name] != val, csv_file))
+    except Exception as e:
+        return f"Строка со значением {val} поля {col_name} не найдена"
+    return f"Строка со значением {val} поля {col_name} удалена!"
+
+# Поиск по пробегу
+def find(val, col_name='пробег'):
+    print(*list(filter(lambda x: x[col_name] == val, csv_file)))
+
+# Поиск по модели
+def find(val, col_name='модель'):
+    print(*list(filter(lambda x: x[col_name] == val, csv_file)))
+
+# поиск по году
+def find(val, col_name='год'):
+    print(*list(filter(lambda x: x[col_name] > val, csv_file)))
+
+
+# Функция Сохранить
+def save(fine_name):
+    with open(fine_name, 'w', encoding='utf-8', newline='') as file:
+        columns = ['vin', 'номер', 'модель', 'год', 'мощность', 'пробег', 'владелец', 'стоимость']
+        writer = csv.DictWriter(file, delimiter=";", fieldnames=columns)
+        writer.writeheader()
+        writer.writerows(csv_file)
+        print("Данные сохранены")
+
+
+# Вывод списка записей
+def show_rows():
+    if len(csv_file) > 0:
+        print('{:<5}{:<15}{:<25}{:<8}{:<12}{:<12}{:<25}{:<12}'.format('vin', 'номер', 'модель', 'год', 'мощность',
+                                                                      'пробег', 'владелец', 'стоимость'))
+        for el in csv_file:
+            print('{:<5}{:<15}{:<25}{:<8}{:<12}{:<12}{:<25}{:<12}'.format(el['vin'], el['номер'], el['модель'],
+                                                                          el['год'], el['мощность'], el['пробег'],
+                                                                          el['владелец'], el['стоимость']))
---
+++ main.py
@@ -0,0 +1,44 @@
+from avto_csv import file_open, insert, show_rows, save, drop_by_arg, find
+
+FILENAME = "Data.csv"
+
+MENU = {
+    '1': 'Открыть файл',
+    '2': 'Добавить',
+    '3': 'Удалить',
+    '4': 'Найти по пробегу',
+    '5': 'Найти по модели',
+    '6': 'Вывести по году',
+    '7': 'Сохранить в файл',
+    '8': 'Вывести записи',
+    '0': '<-Меню',
+    'exit': 'Выход'
+}
+for k, v in MENU.items():
+    print(k, '-', v)
+
+while True:
+
+    action = input('>_')
+    if action == '1':
+        file_open(FILENAME)
+    elif action == '2':
+        insert(input('vin: '), input('номер: '), input('модель: '), input('год: '),
+               input('мощность: '), input('пробег: '), input('владелец: '), input('стоимость: '))
+    elif action == '3':
+        print(drop_by_arg(input("Значение: "), input("Колонка: ")))
+    elif action == '4':
+        find(input("Значение: "), input("Колонка: "))
+    elif action == '5':
+        find(input("Значение: "), input("Колонка: "))
+    elif action == '6':
+        find(input("Значение: "), input("Колонка: "))
+    elif action == '7':
+        save(FILENAME)
+    elif action == '8':
+        show_rows()
+    elif action == '0':
+        for k, v in MENU.items():
+            print(k, '-', v)
+    elif action == 'exit':
+        break
